d12d29adfe313bfa5a30aee694b8201bda64c48f
Vishesh Mehrotra <meh.vishesh1998@gmail.com>
2023-02-17 10:33:59 +0530

    Added REACT ROUTER to the web page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import React, {useState} from 'react';
 import Alert from "./components/Alert";
+import About from './components/About';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   const [mode , setMode] = useState('light');
   const [alert , setAlert] = useState('null');
@@ -34,11 +36,21 @@ function App() {
 }
   return (
     <>
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
+
     <div className="container my-3">
-    <Textform showAlert ={showAlert} heading ="Enter the text to analyze below" mode={mode} toggleMode={toggleMode}/>
+    <Routes>
+          <Route exact path="/about"
+            element={<About />}/>
+          
+          <Route exact path="/"
+          element={<Textform showAlert ={showAlert} heading ="Enter the text to analyze below" mode={mode} toggleMode={toggleMode}/>}/>
+          
+        </Routes>
     </div>
+    </Router>
     </>
 
   );
